f7fcbb6641b85ae47d3c84863d76aa4b660379e7
Jonathan Choi <Jonathan.Junghoon.Choi@gmail.Com>
2020-02-09 20:51:39 -0500

    inquirer prompts created

--- index.js
+++ index.js
@@ -20,16 +20,107 @@ var connection = mysql.createConnection({
 connection.connect(function(err) {
   if (err) throw err;
   console.log("connected as id " + connection.threadId);
-  // Where you would do stuff when connection is established
-
-  
-  afterConnection();
+  start()
 });
 
-function afterConnection() {
-  connection.query("SELECT * FROM employee", function(err, res) {
-    if (err) throw err;
-    console.log(res);
-    connection.end();
-  });
+// function afterConnection() {
+//   connection.query("SELECT * FROM department", function(err, res) {
+//     if (err) throw err;
+//     console.log(res);
+//     connection.end();
+//   });
+// }
+
+// function which prompts the user for what action they should take
+function start() {
+  inquirer
+    .prompt({
+      name: "startOptions",
+      type: "list",
+      message: "Would you like to [Add Department/Roles/Employees], [View Departments/Roles/Employees], or [Update employee roles]?",
+      choices: ["Add Department/Roles/Employees", "View Departments/Roles/Employees", "Update employee roles", "EXIT"]
+    })
+    .then(function(answer) {
+      // based on their answer, either various functions to add, view, or update
+      if (answer.startOptions === "Add Department/Roles/Employees") {
+        addDRE();
+      }
+      else if(answer.startOptions === "View Departments/Roles/Employees") {
+        viewDRE();
+      }
+      else if(answer.startOptions === "Update employee roles"){
+        updateR();
+      } else{
+        connection.end();
+      }
+    });
+}
+
+// Function which prompts user for Adding Departments, Adding Roles, and Adding Roles
+function addDRE() {
+  inquirer
+    .prompt({
+      name: "addOptions",
+      type: "list",
+      message: "Would you like to [Add Department], [Add Role], or [Add Employee]?",
+      choices: ["Add Department", "Add Role", "Add Employee", "EXIT"]
+    })
+    .then(function(answer) {
+      // based on their answer, call the bid or the post functions
+      if (answer.addOptions === "Add Department") {
+        addD();
+      }
+      else if(answer.addOptions === "Add Role") {
+        addR();
+      }
+      else if(answer.addOptions === "Add Employee"){
+        addE();
+      } else{
+        connection.end();
+      }
+    });
+}
+
+// Function for prompting user for displaying Departments, Roles, and Employees
+function viewDRE() {
+  inquirer
+    .prompt({
+      name: "viewOptions",
+      type: "list",
+      message: "Would you like to [View Department], [View Role], or [View Employee]?",
+      choices: ["View Department", "View Role", "View Employee", "EXIT"]
+    })
+    .then(function(answer) {
+      // based on their answer, call the various view functions
+      if (answer.viewOptions === "View Department") {
+        viewD();
+      }
+      else if(answer.viewOptions === "View Role") {
+        viewR();
+      }
+      else if(answer.viewOptions === "View Employee"){
+        viewE();
+      } else{
+        connection.end();
+      }
+    });
+}
+
+// Function for prompting user for updating Role
+function updateR() {
+  inquirer
+    .prompt({
+      name: "updateOptions",
+      type: "list",
+      message: "Would you like to [Update Role]?",
+      choices: ["Update Role", "EXIT"]
+    })
+    .then(function(answer) {
+      // based on their answer, call the various view functions
+      if (answer.updateOptions === "Update Role") {
+        upR();
+      }else{
+        connection.end();
+      }
+    });
 }
